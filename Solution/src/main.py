693019ff80b572840ad7b3f0ca99dc1c76648d9a
mikecroall <mikebcroall@gmail.com>
2017-01-04 16:39:41 +0000

    w1 and w2 simultaneously

--- Solution/src/main.py
+++ Solution/src/main.py
@@ -1,17 +1,22 @@
 import numpy as np
 import cv2
 
-original_image, working_image = None, None
+original_image_w1, working_image_w1 = None, None
+original_image_w2, working_image_w2 = None, None
 relative_image_folder_path = "../../BBBC010_v1_images/"
 image_names = None
-current_image_index = -1
-current_image_path = ""
+current_index = -1
+current_w1_path = ""
+current_w2_path = ""
 
 
-def load_image(image_path):
-    global working_image, original_image
-    working_image = cv2.imread(image_path, 0)  # 0 for gray scale
-    original_image = working_image[::]
+def load_images(w1_path, w2_path):
+    global working_image_w1, original_image_w1
+    global working_image_w2, original_image_w2
+    working_image_w1 = cv2.imread(w1_path, 0)  # 0 for gray scale
+    original_image_w1 = working_image_w1[::]
+    working_image_w2 = cv2.imread(w2_path, 0)  # 0 for gray scale
+    original_image_w2 = working_image_w2[::]
     return
 
 
@@ -23,20 +28,28 @@ def load_image_paths():
 
 
 def cycle_images():
-    global current_image_index, current_image_path
-    current_image_index = (current_image_index + 1) % len(image_names)
-    current_image_path = relative_image_folder_path + image_names[current_image_index]
-    load_image(current_image_path)
+    global current_index, current_w1_path, current_w2_path
+    current_index = (current_index + 1) % len(image_names)
+    current_w1_path = relative_image_folder_path + image_names[current_index]
+    current_index = (current_index + 1) % len(image_names)
+    current_w2_path = relative_image_folder_path + image_names[current_index]
+    load_images(current_w1_path, current_w2_path)
     return
 
 
-def load_and_process_next_image():
-    global working_image
+def load_and_process_next_images():
+    global working_image_w1, working_image_w2
     cycle_images()
-    # working_image = adjust_gamma(working_image, 6.5)
-    working_image = cv2.equalizeHist(working_image)
-    # tasks from the .docx
+    working_image_w1 = process_image(working_image_w1)
+    working_image_w2 = process_image(working_image_w2)
     return
+
+
+def process_image(img):
+    # img = adjust_gamma(img, 6.5)
+    img = cv2.equalizeHist(img)
+    # todo tasks from the .docx
+    return img
 
 
 def adjust_gamma(image, gamma=1.0):
@@ -48,10 +61,10 @@ def adjust_gamma(image, gamma=1.0):
 
 
 def mouse_press_callback(event, x, y, flags, param):
-    global working_image
+    global working_image_w1
     if event == cv2.EVENT_LBUTTONDOWN:
-        print("Pos:{},{} B:{} G:{} R:{}".format(x, y, working_image[y, x, 0], working_image[y, x, 1],
-                                                working_image[y, x, 2]))
+        print("Pos:{},{} B:{} G:{} R:{}".format(x, y, working_image_w1[y, x, 0], working_image_w1[y, x, 1],
+                                                working_image_w1[y, x, 2]))
 
 
 main_window_name = "n - cycle images, x - exit"
@@ -62,11 +75,13 @@ cycle_images()
 keep_processing = True
 
 while keep_processing:
-    original_and_working = np.hstack((original_image, working_image))
-    cv2.imshow(main_window_name, original_and_working)
+    w1 = np.hstack((original_image_w1, working_image_w1))
+    w2 = np.hstack((original_image_w2, working_image_w2))
+    both = np.vstack((w1, w2))
+    cv2.imshow(main_window_name, both)
 
     key = cv2.waitKey(40) & 0xFF
     if key == ord('x'):
         keep_processing = False
     elif key == ord('n'):
-        load_and_process_next_image()
+        load_and_process_next_images()

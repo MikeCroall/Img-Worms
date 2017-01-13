046cc419c82e77b7fc96a276d3a7e103387a62ab
mikecroall <mikebcroall@gmail.com>
2017-01-13 14:58:45 +0000

    Docstrings and comments galore

--- Solution/src/main.py
+++ Solution/src/main.py
@@ -1,3 +1,28 @@
+"""
+This script was written to work with Python 3.5.2 using OpenCV 3.1.0, matching the DUDE PCs (in the School)
+
+Script is simply run with no command line arguments, and no manual method calls
+
+To run this script, the provided imgs.txt must be in the same directory, and the folders
+    point to by the variable relative_image_folder_path (currently BBBC010_v1_images/, two directories above)
+    and the variable relative_image_output_folder_path (currently img_out/, two directories above)
+    must be placed correctly relative to the script, or the relative paths adjusted to match their locations.
+    As supplied, they will be in the correct places.
+
+imgs.txt is the list of filenames that can be found in the folder BBBC010_v1_images/, except with D24 removed.
+    This is because D24 only had a w1 version, and not a w2 version. This is also stated at the top of imgs.txt
+    within a comment.
+
+The controls for the window that opens to show the images are shown in the title bar, though is there are a few
+    of them, I have outlined them below:
+
+        n - Progress to the next fileID (loads the next w1 and w2 images) (loops back to first ID after last ID)
+        b - Return to previous fileID (loads the previous w1 and w2 images)
+        l - Toggle labels in the bottom left of each image displayed
+        s - Save the currently displayed w1 and w2 images into the folder pointed at by
+                relative_image_output_folder_path
+        x - exit the window, ending the script
+"""
 import numpy as np
 import cv2
 
@@ -6,7 +31,11 @@ original_image_w2, working_image_w2 = None, None
 relative_image_folder_path = "../../BBBC010_v1_images/"
 relative_image_output_folder_path = "../../img_out/"
 original_file_name = ""
-image_names = None
+
+with open("imgs.txt", "r") as image_names_file:
+    # ignore comments (lines beginning with # )
+    image_names = [x for x in image_names_file.read().split("\n") if not x.lstrip().startswith("#")]
+
 current_index = -1
 current_w1_path = ""
 current_w2_path = ""
@@ -17,38 +46,73 @@ label_images = True
 
 
 def load_images(w1_path, w2_path):
+    """
+        Take two file paths and load the image files that the paths point at into global variables
+        working_image_w1, working_image_w2 for manipulation, also saving a copy of each original
+        in global variables original_image_w1, original_image_w2.
+
+        Keyword arguments:
+        w1_path -- the file path to the w1 version of a file
+        w2_path -- the file path to the w2 version of a file
+    """
     global working_image_w1, original_image_w1
     global working_image_w2, original_image_w2
 
     # -1 for importing as is (16bit), right shift it then make 8bit to extract more information than importing as 8bit
     w1img16 = cv2.imread(w1_path, -1)
+    # ignore 4 least significant bits to be able to use more of the more significant bits
     w1img16b = np.right_shift(w1img16, 4)
+    # convert to 8bit
     working_image_w1 = np.array(w1img16b, dtype=np.uint8)
+    # save copy for comparing original with processed
     original_image_w1 = working_image_w1.copy()
 
     # -1 for importing as is (16bit), right shift it then make 8bit to extract more information than importing as 8bit
     w2img16 = cv2.imread(w2_path, -1)
+    # ignore 4 least significant bits to be able to use more of the more significant bits
     w2img16b = np.right_shift(w2img16, 4)
+    # convert to 8bit
     working_image_w2 = np.array(w2img16b, dtype=np.uint8)
+    # save copy for comparing original with processed
     original_image_w2 = working_image_w2.copy()
     return
 
 
 def cycle_images():
-    global current_index, current_w1_path, current_w2_path, original_file_name
+    """
+        Finds the next two image filenames to be loaded, constructs the relative file path to them,
+        and sends these to load_images(...). Also prints current FileID for reference.
+    """
     # Not ALL images have a w1 AND a w2 version - D24 for example has no w2, and so is EXCLUDED from imgs.txt
+    global current_index, current_w1_path, current_w2_path, original_file_name
+    # progress one image, looping if needed
     current_index = (current_index + 1) % len(image_names)
+    # save path to this image as w1
     current_w1_path = relative_image_folder_path + image_names[current_index]
+    # progress one image, looping if needed
     current_index = (current_index + 1) % len(image_names)
+    # save path to this image as w2
     current_w2_path = relative_image_folder_path + image_names[current_index]
+    # save original_file_name for fileID isolation later # todo save just fileID?
     original_file_name = str(image_names[current_index])
+
     print("Loading images marked {}".format(original_file_name[33:36]))
+    # actually load the images from the found paths
     load_images(current_w1_path, current_w2_path)
     return
 
 
 def save_images(w1, w2):
+    """
+        Take two image objects, and save them to file in the folder pointed at by
+        relative_image_output_folder_path.
+
+        Keyword arguments:
+        w1 -- image object to be saved to file as fileid_w1.jpg
+        w2 -- image object to be saved to file as fileid_w2.jpg
+    """
     global original_file_name
+    # isolate the fileID, keeping the _ after it
     file_id = original_file_name[-47:-43]
     cv2.imwrite(relative_image_output_folder_path + file_id + "w1.jpg", w1)
     cv2.imwrite(relative_image_output_folder_path + file_id + "w2.jpg", w2)
@@ -57,6 +121,9 @@ def save_images(w1, w2):
 
 
 def load_and_process_next_images():
+    """
+        Cycles to the next pair of images, processes them, and handles saving once processed if variable set
+    """
     global working_image_w1, working_image_w2
     cycle_images()
     working_image_w1 = process_image(working_image_w1, True)
@@ -67,7 +134,16 @@ def load_and_process_next_images():
 
 
 def process_image(img, is_w1=True):
-    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    """
+        Takes an image, knowing if w1 or w2, and processes it according to the tasks given
+
+        Keyword arguments:
+        img -- image object to process
+        is_w1 -- should be True if img is w1 version, False otherwise
+    """
+
+    # create binary image, where worms are white, background is black
+    img_bin = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
 
     # todo next, contour everything, and ignore any with area bigger than or smaller than the worms,
     # todo       eliminating borders and noise hopefully
@@ -76,42 +152,57 @@ def process_image(img, is_w1=True):
 
     # maybe useful:
     # http://docs.opencv.org/trunk/d3/db4/tutorial_py_watershed.html
-    return img
+    return img_bin
 
 
-main_window_name = "n - cycle images, l - toggle labels, s - save current images, x - exit"
+# prepare window to display results next to originals
+main_window_name = "n - next, b - previous, l - toggle labels, s - save current images, x - exit"
 cv2.namedWindow(main_window_name, cv2.WINDOW_NORMAL)
-with open("imgs.txt", "r") as image_names_file:
-    image_names = [x for x in image_names_file.read().split("\n") if not x.lstrip().startswith("#")]
 load_and_process_next_images()
 
+# until x pressed
 while keep_processing:
-    w1, w2 = None, None
+    both_w1, both_w2 = None, None
 
     if label_images:
+        # create copies, to allow for removing of labels (don't modify original and working
         o_w1_labelled = original_image_w1.copy()
         o_w2_labelled = original_image_w2.copy()
         w_w1_labelled = working_image_w1.copy()
         w_w2_labelled = working_image_w2.copy()
+        # place labels uniformly in the bottom left of each image
         cv2.putText(o_w1_labelled, 'original w1', (10, 510), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
         cv2.putText(o_w2_labelled, 'original w2', (10, 510), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
         cv2.putText(w_w1_labelled, 'processed w1', (10, 510), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
         cv2.putText(w_w2_labelled, 'processed w2', (10, 510), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
-        w1 = np.hstack((o_w1_labelled, w_w1_labelled))
-        w2 = np.hstack((o_w2_labelled, w_w2_labelled))
+        # combine images for same-window viewing
+        both_w1 = np.hstack((o_w1_labelled, w_w1_labelled))
+        both_w2 = np.hstack((o_w2_labelled, w_w2_labelled))
     else:
-        w1 = np.hstack((original_image_w1, working_image_w1))
-        w2 = np.hstack((original_image_w2, working_image_w2))
+        # combine images for same-window viewing
+        both_w1 = np.hstack((original_image_w1, working_image_w1))
+        both_w2 = np.hstack((original_image_w2, working_image_w2))
 
-    both = np.vstack((w1, w2))
+    # final combining of images for same-window viewing
+    both = np.vstack((both_w1, both_w2))
+    # actually show
     cv2.imshow(main_window_name, both)
 
+    # wait for any given commands
     key = cv2.waitKey(40) & 0xFF
     if key == ord('x'):
+        # user wants to exit
         keep_processing = False
     elif key == ord('n'):
+        # progress through images
+        load_and_process_next_images()
+    elif key == ord('b'):
+        # reverse through images
+        current_index -= 4  # back 4, cycle_images() steps forward twice and handles looping for us
         load_and_process_next_images()
     elif key == ord('l'):
+        # toggle label option
         label_images = not label_images
     elif key == ord('s'):
+        # trigger save procedure
         save_images(working_image_w1, working_image_w2)
